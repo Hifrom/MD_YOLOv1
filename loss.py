d1f4cd26858c71da678c21a7d7f016d6e6af3328
Dmitry <80029398+Hifrom@users.noreply.github.com>
2021-03-09 22:57:54 +0300

    Loss function

--- loss.py
+++ loss.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from utils import intersection_over_union
 
-
 class YoloLoss(nn.Module):
     def __init__(self, S=7, B=2, C=1):
         super(YoloLoss, self).__init__()
